d2bfa67990b43e1d77d8a9a981dcfb0b8a379e7b
Benedikt Linse <benedikt@confluent.io>
2024-12-17 21:00:59 +0100

    new version: 0.0.12

--- deployment/submit-build.sh
+++ deployment/submit-build.sh
@@ -2,7 +2,7 @@ set -u
 set -e
 
 REPO=us-central1-docker.pkg.dev/solutionsarchitect-01/flink-replication-anonymization
-VERSION=0.0.11
+VERSION=0.0.12
 
 cp ../target/flink-replication-anonymization-1.0-SNAPSHOT.jar docker/
 gcloud builds submit \
